58662d257e6d8a1e4890d631fed73f695e47836b
machadopy <vitorandroidcwb@gmail.com>
2025-11-04 07:29:18 -0300

    database

--- Projeto pessoal - FuelRequest/FuelRequestsCore/admin.py
+++ Projeto pessoal - FuelRequest/FuelRequestsCore/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . models import  cliente, plano
 
 # Register your models here.
+
+class planoAdmin(admin.ModelAdmin):
+    list_display=('nome', 'preco', 'vendas')
+
+
+class clienteAdmin(admin.ModelAdmin):
+    list_display=('nome', 'sobrenome', 'email')
+
+
+admin.site.register(cliente, clienteAdmin)
+admin.site.register(plano, planoAdmin)
--- Projeto pessoal - FuelRequest/FuelRequestsCore/models.py
+++ Projeto pessoal - FuelRequest/FuelRequestsCore/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
+class plano(models.Model):
+    nome = models.CharField('Nome', max_length=32)
+    preco = models.DecimalField('Preco', decimal_places=2, max_digits=8)
+    vendas = models.IntegerField('vendas', validators =[MinValueValidator(0)])
+
+    def __str__(self):
+        return self.nome
+
+
+class cliente(models.Model):
+    nome = models.CharField('nome', max_length=64)
+    sobrenome = models.CharField('sobrenome', max_length=32)
+    email = models.EmailField('email', max_length=100)
+
+    def __str__(self):
+        return self.nome
