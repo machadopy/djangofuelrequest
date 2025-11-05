7e1d07d8f301984fa931f0ae8c9d171811bb4e95
machadopy <vitorandroidcwb@gmail.com>
2025-11-05 00:52:49 -0300

    endpoints with pks

--- Projeto pessoal - FuelRequest/FuelRequestsCore/views.py
+++ Projeto pessoal - FuelRequest/FuelRequestsCore/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
+from . import models as cr
 
-# Create your views here.
+
+
 def index(request):
+    todos_os_planos = cr.plano.objects.all()
     context = {
-        'planos': 'Basico'
+        'planos': todos_os_planos
     }
     return render(request, 'index.html', context)
 
-def contato(request):
+def ver_plano(request, pk):
+    plans = cr.plano.objects.get(id=pk)
+    context={
+        'plano': plans
+    }
+    return render(request, 'plano.html', context)
+
+
+def contato(request,pk):
     return render(request, 'contato.html')
+
