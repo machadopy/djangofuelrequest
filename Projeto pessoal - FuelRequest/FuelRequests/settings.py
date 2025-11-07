5970808cd406ac3aa52ec8214a0359a1f6cfa03a
machadopy <vitorandroidcwb@gmail.com>
2025-11-06 21:00:03 -0300

    staticfiles

--- Projeto pessoal - FuelRequest/FuelRequests/settings.py
+++ Projeto pessoal - FuelRequest/FuelRequests/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = 'django-insecure-akv%i$u4yoya9y(a=@vf7(xe443@9-y9@2olg14%%2-y%n^&7(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 
+    'localhost',
+    '127.0.0.1',
+    ]
 
 
 # Application definition
@@ -118,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- Projeto pessoal - FuelRequest/FuelRequestsCore/views.py
+++ Projeto pessoal - FuelRequest/FuelRequestsCore/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from . import models as cr
 
+from django.http import HttpResponse
+
+from django.template import loader
 
 
 def index(request):
@@ -21,3 +24,12 @@ def ver_plano(request, pk):
 def contato(request,pk):
     return render(request, 'contato.html')
 
+
+def error404(request, exception):
+    template = loader.get_template('error404.html')
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf8', status=404)
+
+
+def error500(request, exception):
+    template = loader.get_template('error500.html')
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf8', status=500)
